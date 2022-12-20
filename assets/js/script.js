ef2c1ac83ea00b690d503e1a5e9edb4f4369f59b
karim atef <96080998+karim5397@users.noreply.github.com>
2022-12-20 10:04:21 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -331,6 +331,25 @@ $('.certificat-owl-carousel').owlCarousel({
 })
 
 
+$('.client-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+	autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+        },
+        600:{
+            items:4,
+        },
+        1000:{
+            items:8,
+        }
+    }
+})
+
 $('.single-project').owlCarousel({
     loop:true,
     margin:10,
